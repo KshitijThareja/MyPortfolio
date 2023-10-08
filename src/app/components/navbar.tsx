994a1a09f42de93ea4de30c0c335ac0c6ea2defb
kshitijthareja <kshitijthareja03@gmail.com>
2023-10-08 13:39:59 +0530

    Add custom cursor

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,6 +6,8 @@ import { Saira_Stencil_One } from "next/font/google";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from "next/link";
+import "../../app/cursor.css"
 library.add(faBarsStaggered)
 const saira= Saira_Stencil_One({
   subsets:['latin'],
@@ -22,13 +24,13 @@ function Navbar() {
         <h1 className={'text-4xl text-white uppercase ml-8'}>KSHITIJ<span className=" text-green-400 "> .</span></h1></main>
         
         <nav
-          className={`w-full lg:flex lg:items-center lg:w-auto ${
+          className={`w-full lg:flex lg:items-center lg:w-auto ml-7 ${
             open ? "block" : "hidden"
           }`}
         >
           
-        <ul className="text-base text-gray-400 lg:flex lg:justify-between list-none">
-          <li className="lg:px-6 py-2 block hover:text-white text-lg ml-9 font-semibold hover:scale-110 ease-in duration-300">Home</li>
+        <ul id="links" className="text-base text-gray-400 lg:flex lg:justify-between list-none">
+          <li className="lg:px-6 py-2 block hover:text-white text-lg ml-9 font-semibold hover:scale-110 ease-in duration-300"><Link id="Link" href="/">Home</Link></li>
           <li className="lg:px-6 py-2 block hover:text-white text-lg font-semibold hover:scale-110 ease-in duration-300">Pages</li>
           <li className="lg:px-6 py-2 block hover:text-white text-lg font-semibold hover:scale-110 ease-in duration-300">Services</li>
           <li className="lg:px-6 py-2 block hover:text-white text-lg font-semibold hover:scale-110 ease-in duration-300">Portfolio</li>
@@ -41,9 +43,11 @@ function Navbar() {
         <div>
        
   </div>
-  <div className="mt-2"><DarkMode/></div>
-    <div>
-      <FontAwesomeIcon width={40} height={40} className="fa-xl mr-6" color="#faf7f0" icon={"bars-staggered"} />
+  
+    <div className="grid grid-cols-3">
+    <div className="col-span-1"><DarkMode/></div>
+    <div className="col-span-1 "></div>
+      <div className="col-span-1 mt-1"><FontAwesomeIcon className="fa-xl" color="#faf7f0" icon={"bars-staggered"} /></div>
     </div>
       </div>
       
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
+'use client'
 import Navbar from "./components/navbar";
 import Link from "next/link";
+import "../app/cursor.css"
+import { useEffect } from "react";
+function LandingPage () {
+  useEffect(() => {
+    var cursor = document.querySelector( "#cursor" );
+            var cursor2 = document.querySelector( "#cursor2" );
 
-const LandingPage = () => {
+            document.addEventListener( "mousemove", function ( e )
+            {
+                cursor.style.cssText = cursor2.style.cssText = "left:" + e.clientX + "px;" + "top:" + e.clientY + "px;";
+            } )
+  }, [])
   return (
-    <div className="">
+    <div id="text">
+      
+      <div id="cursor"></div>
+      <div id="cursor2"></div>
+
       <Navbar/>
+      
     </div>
   );
 };
