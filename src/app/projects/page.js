92fbe46df30ad17c00bf9533ea79d5b9db192242
KshitijThareja <kshitijthareja03@gmail.com>
2024-03-27 23:04:54 +0000

    Update projects list

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -160,6 +160,26 @@ const projects = () => {
         <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60"><Link href="/" passHref target="_blank">See More</Link></button>
       </div>
     </div></Tilt>
+
+    <Tilt
+                  glareEnable={true}
+                  tiltMaxAngleX={20}
+                  tiltMaxAngleY={20}
+                  perspective={1500}
+                  glareColor={isDarkmode ? "black" : "white"}
+                >
+    <div className="group relative cursor-pointer items-center justify-center overflow-hidden rounded-xl transition-shadow hover:shadow-xl hover:shadow-black/30">
+      <div className="h-96 w-72">
+        <img className="h-full w-full object-fit transition-transform duration-500 group-hover:rotate-3 rounded-xl group-hover:scale-125" src="./cvyv8n.png" alt="" />
+      </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black rounded-xl group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+      <div className="absolute inset-0 flex hide1:mb-4 md:mb-4 lg:mb-0 translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+        <h1 className="font-dmserif text-3xl font-bold text-white">CCTV People Detection</h1>
+        <p className="mb-3 italic text-white text-[1.4rem] opacity-0 transition-opacity duration-300 group-hover:opacity-100">A webapp to detect and count number of people in a CCTV footage using computer vision.</p>
+        <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60"><Link href="/" passHref target="_blank">See More</Link></button>
+      </div>
+    </div></Tilt>
+
   </div>
 </div>
 
